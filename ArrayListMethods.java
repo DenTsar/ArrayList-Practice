28e7feeeb0376ddc555e3bc6d11abc9c382894bc
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-11-27 12:30:43 -0500

    main method
    added methods to main method to test them

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
 public class ArrayListMethods{
 	public static void main(String[]args){
+		ArrayList<Integer> a = new ArrayList<>();
+		ArrayList<Integer> b = new ArrayList<>();
+		for(int i = 0; i<2; i++)
+			for(int j = 0; j<10; j++)
+				a.add(j);
+		for(int i = 20; i>12; i--)
+			b.add(i);
+		System.out.println(combine(a,b));
+		print(a);
+		System.out.println(removeDuplicates(a));
+
 	}
 	public static ArrayList<Integer> combine(ArrayList<Integer> a, ArrayList<Integer> b){
 		for(int i = 0; i<b.size(); i++)
