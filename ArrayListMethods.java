90520d0763f4cc9f353eefa086e0ed683c219e60
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-11-27 12:19:48 -0500

    first method
    addded mthod that combines two integer arraylists

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
 public class ArrayListMethods{
 	public static void main(String[]args){
+	}
+	public static ArrayList<Integer> combine(ArrayList<Integer> a, ArrayList<Integer> b){
+		for(int i = 0; i<b.size(); i++)
+			a.add(b.get(i));
+		return a;
 	}
 }
